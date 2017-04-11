9322f08516376ef705ba0a50b6677a77f6d72dab
bart74 <bart.74@hotmail.fr>
2017-04-11 22:20:20 +0200

    p'tite upate

--- index.php
+++ index.php
@@ -5,23 +5,6 @@ include('header.html');
 
 <!--insert some markup-->
 
-<h1>Titre principal,bienvenue et tutti quanti</h1>
-
-<h2>Un site complet!</h2>
-Ce site contiendra du contenu,du contenu et encore plus de contenu.
-<br/>
-Ce site est la perfection.
-<br/>
-Ce site est le meilleur de tout les sites.
-
-<h2 class='right'>Avec des titres de tout les côtés</h2>
-<p class='right'>
-Avec même les paragraphes qui suivent dit donc
-</p>
-
-<h2>Et encore plus de titres</h2>
-si on arrive pas à faire une page d'accueil bien avec ça dit donc...
-
 <?php
 include('footer.html');
 ?>
