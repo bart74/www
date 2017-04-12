fe10bbe37ed50eb6a66479bd00bf7c86c0393460
bart74 <bart.74@hotmail.fr>
2017-04-12 14:36:34 +0200

    update

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
-$title = 'accueil';
-include('template/header.html');
+$title = 'Accueil';
+include('/template/header.html');
 ?>
 
 <!--insert some markup-->
+<h2>Bienvenue!</h2>
+<p></p>
 
 <?php
-include('template/footer.html');
+include('/template/footer.html');
 ?>
