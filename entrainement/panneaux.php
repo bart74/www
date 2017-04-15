897288a2b95b52a14f23261e9e8c51139cd04c50
bart74 <bart.74@hotmail.fr>
2017-04-15 16:35:51 +0200

    panneaux

--- entrainement/panneaux.php
+++ entrainement/panneaux.php
@@ -9,24 +9,24 @@ include('../template/header.html');
     <th>Signification</th>
   </tr>
   <tr>
-    <td class='panneau'><!--img--></td>
-    <td class='desc'>blabla</td>
+    <td class='panneau'><img src='/ressources/panneaux/sign1.png' alt='un panneau'/></td>
+    <td class='desc'>Dromadaire</td>
   </tr>
   <tr>
-    <td class='panneau'><!--img--></td>
-    <td class='desc'>blabla</td>
+    <td class='panneau'><img src='/ressources/panneaux/sign2.png' alt='un panneau'/></td>
+    <td class='desc'>Chameau</td>
   </tr>
   <tr>
-    <td class='panneau'><!--img--></td>
-    <td class='desc'>blabla</td>
+    <td class='panneau'><img src='/ressources/panneaux/sign3.png' alt='un panneau'/></td>
+    <td class='desc'>MICHAEL FUCKING BAY!</td>
   </tr>
   <tr>
-    <td class='panneau'><!--img--></td>
-    <td class='desc'>blabla</td>
+    <td class='panneau'><img src='/ressources/panneaux/sign4.png' alt='un panneau'/></td>
+    <td class='desc'>Beeeeeeeh</td>
   </tr>
   <tr>
-    <td class='panneau'><!--img--></td>
-    <td class='desc'>blabla</td>
+    <td class='panneau'><img src='/ressources/panneaux/sign5.png' alt='un panneau'/></td>
+    <td class='desc'>Trompettes interdites</td>
   </tr>
   <tr>
     <td class='panneau'><!--img--></td>
