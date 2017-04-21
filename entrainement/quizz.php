a97340ceed94cc029bfdb36e72c3f4fd43959fab
bart74 <bart.74@hotmail.fr>
2017-04-21 04:48:49 +0200

    experimentations css

--- entrainement/quizz.php
+++ entrainement/quizz.php
@@ -3,6 +3,9 @@ $title = "Quizz";
 include('../template/header.html');
 ?>
 <!--insert some markup-->
+
+
+
 <?php
 include('../template/footer.html');
 ?>
--- permis/media.php
+++ permis/media.php
@@ -3,6 +3,9 @@ $title = 'Medias';
 include('../template/header.html');
 ?>
 <!--insert some markup-->
+
+<iframe class='fullscreen' src="https://www.youtube.com/embed/ygr5AHufBN4?autoplay=1"></iframe>
+
 <?php
 include('../template/footer.html');
 ?>
