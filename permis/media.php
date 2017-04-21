b07d19f9ab5a54bb9bac09affad78ae5c8cedc36
bart74 <bart.74@hotmail.fr>
2017-04-21 05:05:07 +0200

    mieux

--- permis/media.php
+++ permis/media.php
@@ -4,7 +4,7 @@ include('../template/header.html');
 ?>
 <!--insert some markup-->
 
-<iframe class='fullscreen' src="https://www.youtube.com/embed/ygr5AHufBN4?autoplay=1"></iframe>
+<iframe class='fullscreen' src="https://www.youtube.com/embed/ygr5AHufBN4?autoplay=1&playlist=ygr5AHufBN4&loop=1&controls=0"></iframe>
 
 <?php
 include('../template/footer.html');
